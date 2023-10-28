0228449f2c4fd325691374701fd7caab921df0fa
s485950 <maksie1@st.amu.edu.pl>
2023-10-28 15:02:33 +0200

    "cwiczenia 1 so far"

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,14 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
+
         System.out.println("Hello! Witaj!");
+        Logger log = Logger.getLogger("name");
+
+        log.info("message");
     }
 }
